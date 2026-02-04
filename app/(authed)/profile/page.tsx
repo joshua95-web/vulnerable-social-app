77d8793cf97769cceed0188ad08ba58d83364f94
Josh <joshcscurfield@gmail.com>
2026-02-04 16:37:22 +0000

    Update page.tsx

--- app/(authed)/profile/page.tsx
+++ app/(authed)/profile/page.tsx
@@ -1,5 +1,15 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0";
+
 const Profile = () => {
-  return <div>User Profile</div>;
+  const { user, error, isLoading } = useUser();
+
+  return (
+    <div>
+      <main>
+        <h1>User Profile</h1>
+      </main>
+    </div>
+  );
 };
 export default withPageAuthRequired(Profile);
