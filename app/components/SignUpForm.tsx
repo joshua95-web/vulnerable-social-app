f665c1b717ef5d0054769440fa46ff565705f31d
Josh <joshcscurfield@gmail.com>
2026-02-04 16:44:39 +0000

    update import

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import { CreateUser } from "../actions/CreateUser";
+import { CreateUser } from "../lib/actions/CreateUser";
 
 export default function SignUpForm() {
   const [userName, setUserName] = useState("");
